1077766884b24c8304913cb9927a808d66e10477
Leo Rocha <leonardocar.dm@gmail.com>
2021-02-01 03:34:16 -0400

    Change vscode workspace settings Change server to use old module.exports for compatibility

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,17 @@ import helmet from 'helmet';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-export default polka() // You can also use Express
-	.use(
-    helmet(),
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, (err: Error)  => {
-		if (err) console.log('error', err);
-	});
+const app = polka(); // You can also use Express
+
+app.use(
+  helmet(),
+  compression({ threshold: 0 }),
+  sirv('static', { dev }),
+  sapper.middleware()
+);
+
+app.listen(PORT, (err: Error)  => {
+  if (err) console.log('error', err);
+});
+
+module.exports = app;
